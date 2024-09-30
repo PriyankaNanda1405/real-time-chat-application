ce585ee7f4f658b56ee0a750d29b1914756c3af3
PriyankaNanda1405 <nandapriyanka121@gmail.com>
2024-09-30 12:04:09 +0530

    initial message

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const connectDB = async() => {
+    try {
+        const conn = await mongoose.connect("mongodb://127.0.0.1:27017/message",{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+        process.exit();
+        }    
+    };
+
+    module.exports= connectDB;
