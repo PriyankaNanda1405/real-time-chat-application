a439b5ddc68f4626f106c830dc604936d62e9f31
PriyankaNanda1405 <nandapriyanka121@gmail.com>
2024-10-01 15:27:49 +0530

    initi

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const connectDB = async() => {
     try {
         const conn = await mongoose.connect("mongodb://127.0.0.1:27017/message",{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
         });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
+const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
 
 
 dotenv.config();
@@ -9,18 +11,25 @@ dotenv.config();
 connectDB();
 const app = express();
 
+app.use(express.json()); //to access json data
+
 app.get('/', (req, res) =>{
     res.send("API is running");
 });
-app.get('/api/chat', (req, res) => {
-    res.send(chats);
-});
+// app.get('/api/chat', (req, res) => {
+//     res.send(chats);
+// });
 
-app.get('/api/chat/:id', (req, res) => {
-    // console.log(req);
-    const singleChat= chats.find(c=>c._id === req.params.id);
-    res.send(singleChat);
-});
+// app.get('/api/chat/:id', (req, res) => {
+//     // console.log(req);
+//     const singleChat= chats.find(c=>c._id === req.params.id);
+//     res.send(singleChat);
+// });
+
+app.use('/api/user',userRoutes)
+
+app.user(notFound)
+app.user(errorHandler)
 const PORT = process.env.PORT || 5000;
 
 app.listen(5000,console.log(`Server started on port ${PORT}`));
